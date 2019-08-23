9fb7595b7b42416b63e4797c5173ae632693eec3
WD$ <wyntonmdavis@gmail.com>
2019-08-23 06:31:35 -0400

    initializing 1st button

--- gif.js
+++ gif.js
@@ -1,6 +1,36 @@
-$("#animal-button").on("click", function(){
 
-    var queryURL ="https://api.giphy.com/v1/gifs/random?api_key=yV94fHMPuqKi6PMkpLJQ5d5TxRmxg5of"
+$("#koala").on("click", function(){
+    
+    var person = $(this).attr("data-person");
+    
+    var queryURL ="https://api.giphy.com/v1/gifs/search?q=" + person + 
+    "&api_key=yV94fHMPuqKi6PMkpLJQ5d5TxRmxg5of";
+    
+    
     console.log("Clicked!");
 
-});
+    $.ajax({
+       url: queryURL,
+       method: "GET"
+    })
+    .then(function(response) {
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            
+            var rating = results[i].rating;
+    
+             var p =$("<p>").text("Rating: " + rating)
+
+             var personImage = $("<img>");
+
+             personImage.attr("src", results[i].images.fixed_height.url);
+
+             gifDiv.prepend(p);
+             gifDiv.prepend(personImage);
+
+             $("#images").prepend(gifDiv);
+                }   
+            });
+        });
