e04fffd3cbd9cfd53c810d0e56aef9198b979d8c
WD$ <wyntonmdavis@gmail.com>
2019-08-24 23:41:15 -0400

    adding gifs to buttons

--- javascript.js
+++ javascript.js
@@ -19,14 +19,48 @@ function renderButtons() {
 
 
   }
+  
 
   $("button").on("click", function(){
   
     console.log("clicked");
+    var person = $(this).attr("data-person");
+    
+    var queryURL ="https://api.giphy.com/v1/gifs/search?q=" + person + 
+    "&api_key=yV94fHMPuqKi6PMkpLJQ5d5TxRmxg5of";
+  
+    $.ajax({
+       url: queryURL,
+       method: "GET"
+    })
+   
+    .then(function(response) {
+        
+      var results = response.data;
+  
+        for (var i = 0; i < results.length; i++) {
+
+          if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+            
+            var gifDiv = $("<div>");
+            
+            var rating = results[i].rating;
+    
+             var p =$("<p>").text("Rating: " + rating)
+  
+             var personImage = $("<img>");
+  
+             personImage.attr("src", results[i].images.fixed_height.url);
+  
+             gifDiv.prepend(p);
+             gifDiv.prepend(personImage);
+  
+             $("#gifs").prepend(gifDiv);
+            }   
+          }
+            });
           });
 }
-
-
 
 $("#add-animal").on("click", function(event) {
 
